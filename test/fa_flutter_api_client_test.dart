e7ae2a92a64d9d26c7b7c463d464db0facf8d1af
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-03-09 14:30:15 +0530

    Add sample test cases
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- test/fa_flutter_api_client_test.dart
+++ test/fa_flutter_api_client_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('This is a sample GET-WHEN-THEN test', () {
+    const value = 5;
+    expect(value == 5, true);
+  });
+}
